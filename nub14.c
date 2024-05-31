81488f2d06aaaf45314e90cefc3b424b59533161
alexeyklanyuk52 <alexeyklanyuk52@gmail.com>
2024-05-31 11:06:32 +0700

    dobavili 2 programme work with semafonoms

---
+++ nub14.c
@@ -0,0 +1,31 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+          int semid;
+          char pathname[] = "for_ipc.ipc";
+          key_t key;
+          struct sembuf mybuf;
+          if((key = ftok(pathname, 0)) < 0) {
+                  printf("ne ydalos' generate key ipc\n");
+                  exit(-1);
+          }
+          if((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
+                    printf("ne ydalos' get diskriptor massive semarfoz");
+                    exit(-1);
+          }
+          mybuf.sem_op = -1;
+          mybuf.sem_flg = 0;
+          mybuf.sem_num = 0;
+          printf("waits unlock tekyshwgo procecca\n");
+          if(semop(semid,&mybuf, 1) < 0) {
+               printf("ne ydalos' vepolnit' operac nadsemafonom\n");
+               exit(-1);
+          }
+          printf("tekyshiy procecc unlock\n");
+          return 0;
+}
+
